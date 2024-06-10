3775698e3a4d0e0031433698179b12b1527ebd7a
Lemayian Caleb Murero <102622692+Caleb-Kylib@users.noreply.github.com>
2024-06-10 23:52:47 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app= express()
+const expresslayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set ('layout', 'layouts/layout')
+app.use(expresslayouts)
+app.use(express.static('public'))
+
+app.use('/', indexRouter)
+
+
+app.listen(process.env.PORT || 3000)
